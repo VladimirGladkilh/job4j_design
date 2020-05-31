8b99acbbcf515d3f3ac5032d786263ca62b4f931
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-05-31 23:24:07 +0500

    5.1.4. Создать convert(Iterator<Iterator>)[152#291510]

---
+++ chapter_001/src/main/java/ru/job4j/it/Converter.java
@@ -0,0 +1,29 @@
+package ru.job4j.it;
+
+import java.util.*;
+
+public class Converter {
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+
+        return new Iterator<Integer>() {
+            private Iterator<Integer> integerIterator= (new ArrayList<Integer>()).iterator();
+            @Override
+            public boolean hasNext() {
+                while (it.hasNext() && !integerIterator.hasNext()) {
+                    integerIterator= it.next();
+                }
+                return integerIterator.hasNext();
+            }
+
+            @Override
+            public Integer next() {
+                if (!this.hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return integerIterator.next();
+            }
+        };
+    }
+
+
+}
