a13ed021dc343f6416e440b927ded7725b5b7c10
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-07-07 14:01:40 +0500

    0.1. FileOutputStream.[252488#291549]

---
+++ chapter_001/src/main/java/io/ResultFile.java
@@ -0,0 +1,22 @@
+package io;
+
+import java.io.FileOutputStream;
+import java.util.StringJoiner;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        StringJoiner sj = new StringJoiner(System.lineSeparator());
+        for (int i = 1; i < 10; i++) {
+            StringJoiner sk = new StringJoiner("\t");
+            for (int k = 1; k < 10; k ++) {
+                sk.add(k + "*" + i + "="+ k * i);
+            }
+            sj.add(sk.toString());
+        }
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            out.write(sj.toString().getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
