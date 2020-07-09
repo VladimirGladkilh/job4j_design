8b9aa5c3d96d8a7c03bdf48b68190a58735b8c7e
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-07-09 12:55:54 +0500

    5. Валидация параметров запуска.[246865#291548]

--- chapter_002/src/main/java/io/Search.java
+++ chapter_002/src/main/java/io/Search.java
@@ -2,13 +2,20 @@ package io;
 
 import java.io.IOException;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.List;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, "java").forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER EXT");
+        }
+        if (args.length == 1) {
+            throw new IllegalArgumentException("Searched extension is null. Usage java -jar dir.jar ROOT_FOLDER EXT");
+        }
+        String folder = args[0];
+        String ext = args[1];
+        Path start = Paths.get(folder);
+        search(start, ext).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, String ext) throws IOException {
