e0725728758663c93d58852b308fe5b800afa6a8
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-07-09 10:58:51 +0500

    3.0. Тестирование IO[173905#291547]

--- chapter_002/src/test/java/io/AnalizyTest.java
+++ chapter_002/src/test/java/io/AnalizyTest.java
@@ -1,22 +1,38 @@
 package io;
 
-import org.junit.Test;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
 
 public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void testUnavailable() {
-        String path = "./data/server.log";
-        String out = "./data/unavailable.csv";
+    public void testUnavailable() throws IOException {
+        String path = "server.log";
+        String out = "unavailable.csv";
+        File source = folder.newFile(path);
+        File target = folder.newFile(out);
+        try (PrintWriter outPrintWriter = new PrintWriter(source)) {
+            outPrintWriter.println("200 10:56:01\n" +
+                    "500 10:57:01\n" +
+                    "400 10:58:01\n" +
+                    "200 10:59:01\n" +
+                    "200 11:00:01\n" +
+                    "500 11:01:02\n" +
+                    "200 11:02:02");
+        }
         Analizy analizy = new Analizy();
-        analizy.unavailable(path, out);
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         String firtsStop = "10:57:01; 10:59:01";
         String find = "";
-        try (BufferedReader in = new BufferedReader(new FileReader(out))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(target.getAbsoluteFile()))) {
             find = in.readLine();
         } catch (Exception e) {
             e.printStackTrace();
@@ -24,14 +40,25 @@ public class AnalizyTest {
         assertThat(firtsStop, is(find));
     }
     @Test
-    public void testLast() {
-        String path = "./data/server.log";
-        String out = "./data/unavailable.csv";
+    public void testLast() throws IOException {
+        String path = "server.log";
+        String out = "unavailable.csv";
+        File source = folder.newFile(path);
+        File target = folder.newFile(out);
+        try (PrintWriter outPrintWriter = new PrintWriter(source)) {
+            outPrintWriter.println("200 10:56:01\n" +
+                    "500 10:57:01\n" +
+                    "400 10:58:01\n" +
+                    "200 10:59:01\n" +
+                    "200 11:00:01\n" +
+                    "500 11:01:02\n" +
+                    "200 11:02:02");
+        }
         Analizy analizy = new Analizy();
-        analizy.unavailable(path, out);
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
         String lastStop = "11:01:02; 11:02:02";
         String find = "";
-        try (BufferedReader in = new BufferedReader(new FileReader(out))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(target.getAbsoluteFile()))) {
             find = in.lines().reduce((first, second) -> second).orElse("");
         } catch (Exception e) {
             e.printStackTrace();
