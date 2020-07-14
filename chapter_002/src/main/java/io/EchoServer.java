17d90535c6b01ca911d572ffbc3dcd913e5b9742
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-07-14 13:06:54 +0500

    1. Бот[7921#291564]

--- chapter_002/src/main/java/io/EchoServer.java
+++ chapter_002/src/main/java/io/EchoServer.java
@@ -5,6 +5,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+    static final String COMMAND_EXIT = "exit";
+    static final String COMMAND_HELLO = "hello";
     public static void main(String[] args) throws IOException {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean run = true;
@@ -14,14 +16,24 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    String command="";
                     while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
-                        if (str.indexOf("?msg=Bye ") > 0) {
-                            run = false;
+                        if (str.indexOf("msg=") > 0) {
+                            command = str.split("msg=")[1];
+                            command = command.split("HTTP/1.1")[0];
+                            if (command.toLowerCase().equals(COMMAND_HELLO)) {
+                               command = COMMAND_HELLO;
+                            } else {
+                                if (command.toLowerCase().equals(COMMAND_EXIT)) {
+                                    command = COMMAND_EXIT;
+                                    run = false;
+                                }
+                            }
                         }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
-
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(command.getBytes());
                 }
             }
         }
