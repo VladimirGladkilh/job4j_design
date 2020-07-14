2d5e25e48ebd02595a9eb6f82be7590f4672b3a8
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-07-14 13:47:09 +0500

    4. Slf4j - вывод exception.[268853#291544]

--- chapter_002/src/main/java/io/EchoServer.java
+++ chapter_002/src/main/java/io/EchoServer.java
@@ -4,10 +4,15 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EchoServer {
     static final String COMMAND_EXIT = "exit";
     static final String COMMAND_HELLO = "hello";
-    public static void main(String[] args) throws IOException {
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+    public static void main(String[] args) {
         try (ServerSocket server = new ServerSocket(9000)) {
             boolean run = true;
             while (run) {
@@ -16,14 +21,14 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
-                    String command="";
+                    String command = "";
                     while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
                         if (str.indexOf("msg=") > 0) {
                             command = str.split("msg=")[1];
                             command = command.split("HTTP/1.1")[0];
                             if (command.toLowerCase().equals(COMMAND_HELLO)) {
-                               command = COMMAND_HELLO;
+                                command = COMMAND_HELLO;
                             } else {
                                 if (command.toLowerCase().equals(COMMAND_EXIT)) {
                                     command = COMMAND_EXIT;
@@ -36,6 +41,8 @@ public class EchoServer {
                     out.write(command.getBytes());
                 }
             }
+        }catch (IOException e) {
+            LOG.error("Error in main method", e);
         }
     }
 }
