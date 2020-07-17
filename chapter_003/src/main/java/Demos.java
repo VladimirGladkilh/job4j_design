d90d8935e4b93c1ca4c60aaf97519e4057658090
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-07-17 15:10:47 +0500

    пустой коммит

---
+++ chapter_003/src/main/java/Demos.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Demos {
+    public static void main(String[] args) {
+        List<String> l = new LinkedList<>();
+        l.add("as"); l.add(1, "33"); l.remove("as");
+        l.get(1);
+        l.set(2,"a");
+
+        Set<String> s = new HashSet<>();
+        s.size(); s.add("as"); s.clear(); s.contains("as"); s.hashCode();
+        s.isEmpty(); s.iterator(); s.remove("asd");
+
+        Map<String, String> m = new HashMap<>();
+        m.put("a","aa");
+        m.size(); m.get("a"); m.getOrDefault("a","dfs");
+        m.remove("a");
+        m.entrySet(); m.keySet(); m.clear();m.containsKey("a"); m.containsValue("aa"); m.hashCode(); m.equals("a");
+
+        Map<String, String> t = new TreeMap<>();
+
+    }
+}
