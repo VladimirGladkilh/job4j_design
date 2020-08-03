eefa5d513042b9f70a210277db7a8aa50fc7348a
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-08-03 12:42:25 +0500

    Вопросы[571#291559]

--- chapter_002/src/main/java/io/Shell.java
+++ chapter_002/src/main/java/io/Shell.java
@@ -1,51 +1,31 @@
 package io;
 
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.StringJoiner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Shell {
 
-    private StringJoiner root = new StringJoiner("/");
+    private Stack<String> root = new Stack<>();
 
     public void cd(String path) {
         for (String p : path.split("/")) {
             if (p.equals("..")) {
                 backStep();
             } else {
-                root.add(p);
+                root.push(p);
             }
         }
     }
     private void backStep() {
-        String[] path = root.toString().split("/");
-        root = new StringJoiner("/");
-        for (int i = 0; i < path.length -1; i ++) {
-            root.add(path[i]);
-        }
+        root.pop();
     }
 
     public String pwd() {
-        return "/" + root.toString();
-    }
-
-    public static void main(String[] args) {
-
-        int num = 1290123;
-        long numUpper = (long) num;
-        short numLower = (short) num;
-        System.out.println(numUpper);
-        System.out.println(numLower);
-        isSorted(new int[] {1,2,3});
-    }
-    public static boolean isSorted(int[] array) {
-        for (int i = 0; i < array.length - 1; i ++) {
-            if (array[i] > array[i + 1]) {
-                return false;
-            }
+        if (root.isEmpty() ) {
+            return "/";
         }
-        return true;
+        return '/' + root.stream().collect(Collectors.joining("/"));
     }
+
 }
