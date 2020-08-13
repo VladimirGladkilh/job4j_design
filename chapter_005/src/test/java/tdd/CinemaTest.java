79f4bca70a326c3e94ffa312a66fe7e3f5187288
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-08-13 14:31:36 +0500

    2. Что такое TDD?[4918#291569]

---
+++ chapter_005/src/test/java/tdd/CinemaTest.java
@@ -0,0 +1,62 @@
+package tdd;
+
+import junit.framework.TestCase;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+public class CinemaTest {
+
+    @Test
+    public void buy() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Test
+    public void find() {
+        Cinema cinema = new Cinema3D();
+        cinema.add(new Session3D());
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions, is(Arrays.asList(new Session3D())));
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyOnDateWithoutSession() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2019, 3, 30, 23, 0);
+        Ticket ticket = cinema.buy(account, 2, 123, date);
+        assertNull(ticket);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void whenNoFreePlaces() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2020, 10, 10, 23, 0);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+        assertNull(ticket);
+    }
+
+    @Test
+    public void whenUserExist() {
+        Account account = new AccountCinema();
+        assertNull(account);
+    }
+
+}
