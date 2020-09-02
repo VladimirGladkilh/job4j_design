e9a0e226dc9399436b06e51fc9b164e3ba7c8ae3
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-09-02 16:01:23 +0500

    1. Динамическое перераспределение продуктов[854#291589]

--- chapter_005/src/main/java/lsp/ControlQuality.java
+++ chapter_005/src/main/java/lsp/ControlQuality.java
@@ -9,12 +9,18 @@ public class ControlQuality {
         this.stores = stores;
     }
 
-
+    public void control(List<Food> foodList) {
+        for (Food food : foodList) {
+            for (Store store: this.stores) {
+                store.add(food);
+            };
+        }
+    }
     /**
      * Соберем всю еду в один список и потом повторно проидемся с этим списком по хранилищам
      * правда это увеличит сложность до квадратичной
      */
-    public void control() {
+    public void resort() {
         List<Food> foodList = new LinkedList<>();
         for (Store store: this.stores) {
             List<Food> foods = store.clear();
--- chapter_005/src/test/java/lsp/ControlQualityTest.java
+++ chapter_005/src/test/java/lsp/ControlQualityTest.java
@@ -8,7 +8,35 @@ import java.util.*;
 
 public class ControlQualityTest {
     @Test
-    public void whenStore(){
+    public void whenStoreControl(){
+        Calendar date = Calendar.getInstance();
+        date.set(2020, Calendar.AUGUST, 29, 0, 0, 0);
+        Warehouse warehouse = new Warehouse();
+        warehouse.add(new Drink("Royal", 50, date, 300));
+        Shop shop = new Shop();
+        shop.add(new Drink("Old melnik", 5 , date, 4.5 ));
+        Trash trash = new Trash();
+
+        List<Food> foodList = new LinkedList<Food>(List.of(
+                new Milk("Nasha Masha", 10 , date, 45.5 ),
+                new Milk("House in Village", 30 , date, 45.5 ),
+                new Drink("Fiesta", 3 , date, 405.5 ),
+                new Drink("Beer", 1 , date, 5.5 ),
+                new Drink("Fanta", 150, date, 56.0)));
+
+
+        List<Store> stores = new LinkedList<>();
+        stores.add(warehouse);
+        stores.add(shop);
+        stores.add(trash);
+        ControlQuality cq = new ControlQuality(stores);
+        cq.control(foodList);
+        warehouse.showStore();
+        shop.showStore();
+        trash.showStore();
+    }
+    @Test
+    public void whenStoreResort(){
         Calendar date = Calendar.getInstance();
         date.set(2020, Calendar.AUGUST, 29, 0, 0, 0);
         Warehouse warehouse = new Warehouse();
@@ -30,10 +58,9 @@ public class ControlQualityTest {
         stores.add(shop);
         stores.add(trash);
         ControlQuality cq = new ControlQuality(stores);
-        cq.control();
+        cq.resort();
         warehouse.showStore();
         shop.showStore();
         trash.showStore();
     }
-
 }
