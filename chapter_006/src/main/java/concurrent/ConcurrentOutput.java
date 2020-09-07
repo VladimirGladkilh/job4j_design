e5fbb6f0ce947f9d750b4e7ca0fe4a15dc0a7022
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-09-07 12:10:57 +0500

    1. Запуск нити. Thread#start()[1016#357576]

---
+++ chapter_006/src/main/java/concurrent/ConcurrentOutput.java
@@ -0,0 +1,18 @@
+package concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        Thread second = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        });
+        second.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
