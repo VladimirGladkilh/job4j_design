313d56e85cf66d13a26147d83140cf13a4a240a8
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-09-07 14:00:44 +0500

    3. Прерывание нити[1019#357577]

---
+++ chapter_006/src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,32 @@
+package concurrent;
+
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        int i = 0;
+        String[] process = new String[]{"|", "/", "-", "\\"};
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\rload: " + process[i % 4]);
+            i++;
+        }
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args)  {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        try {
+            Thread.sleep(2000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        progress.interrupt(); //
+        System.out.println("End");
+    }
+}
