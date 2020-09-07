680f253a26469cb380fb780f1f2fe6e0b85b7601
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-09-07 14:07:42 +0500

    3.1. Прерывание блокированной нити.[267413#357575]

--- chapter_006/src/main/java/concurrent/Wget.java
+++ chapter_006/src/main/java/concurrent/Wget.java
@@ -9,7 +9,7 @@ public class Wget {
                    Thread.sleep(100);
                }
            } catch (InterruptedException e) {
-               e.printStackTrace();
+               Thread.currentThread().interrupt();
            }
         });
         first.start();
