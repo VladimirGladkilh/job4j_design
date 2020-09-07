ee677e9a7f94b8d44a009dc3a961f2b2a545d75d
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-09-07 12:27:51 +0500

    1. Хранилище продуктов[852#291582]

---
+++ chapter_006/src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,22 @@
+package concurrent;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(() -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        while (first.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " = " + first.getState());
+        }
+        System.out.println(first.getName() + " = " + first.getState());
+        Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        second.start();
+        while (second.getState() != Thread.State.TERMINATED) {
+            System.out.println(second.getName() + " = " + second.getState());
+        }
+        System.out.println(second.getName() + " = " + second.getState());
+        System.out.println("End work");
+    }
+}
