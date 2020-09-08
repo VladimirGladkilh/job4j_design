188704ea5284a87f8d0de7c44592e715b12caa15
Vladimir Gladkilh <61043208+VladimirGladkilh@users.noreply.github.com>
2020-09-08 11:41:10 +0500

    4. Thread без общих ресурсов[267919#357582]

---
+++ chapter_006/src/main/java/ref/UserCache.java
@@ -0,0 +1,28 @@
+package ref;
+
+import net.jcip.annotations.NotThreadSafe;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@NotThreadSafe
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        ConcurrentHashMap<Integer, User> findList = new ConcurrentHashMap<>();
+        users.values().forEach(user -> findList.put(id.incrementAndGet(), User.of(user.getName())));
+        return new ArrayList<>(findList.values());
+    }
+}
